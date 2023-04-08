73262fbd671fa4617c83ff9b0444351ad9d678c1
Bavanasi <bavanasi_vinitha@columbusstate.edu>
2023-04-08 21:41:07 +0000

    New blazor app

--- MovieTime/Clients/NasaClient.cs
+++ MovieTime/Clients/NasaClient.cs
@@ -1,5 +1,19 @@
 using System.Net.Http.Json;
 using MovieTime.Models;
 
-public class NasaClient{   
+namespace MovieTime.Clients;
+public class NasaClient{
+
+    private readonly HttpClient _client;
+    public NasaClient(HttpClient client){
+        _client=client;
+    }
+public async Task<NasaResponse> GetNasa(string date){
+    var url = "https://api.nasa.gov/planetary/apod?api_key=8gWfQi3EmW5RUs3ffciAZQNhMcsVHFXXGodmIpQg";
+if(date != null){
+ url += "&date=" + date;
+}
+
+    return await _client.GetFromJsonAsync<NasaResponse>(url);
+}
 }
