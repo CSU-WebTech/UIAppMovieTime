24068c2f645003bf732ac92b53e35f6681062943
Chitra Jenish Shah <shah_chitrajenish@columbusstate.edu>
2023-04-26 15:02:39 +0000

    Updated the json properties to get the list of movies and shows

--- MovieTime/Models/SearchMovieResponse.cs
+++ MovieTime/Models/SearchMovieResponse.cs
@@ -9,23 +9,14 @@ public record SearchMovieResponse{
 
 
 public class Searching{
-       // [JsonPropertyName("Title")]
+       
         public string Title {get; set;}
 
-       // [JsonPropertyName("Released")]
         public string Year {get; set;}
         
-       // [JsonPropertyName("Plot")]
-      //  public string explanation {get; init;}
-
-       // [JsonPropertyName("Poster")]
         public string Poster {get; set;}
 
-       // [JsonPropertyName("Director")]
-       // public string director{get; set;}
-
-       // [JsonPropertyName("Actors")]
-       // public string actors{get; set;}      
+        public string Type {get; set;}         
         
 }
 
